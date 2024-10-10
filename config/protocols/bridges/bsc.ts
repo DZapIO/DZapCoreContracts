e8dd1d9736206ffe07c7eb894dd16daba2c24e2e
dzaptech <dezaptech@gmail.com>
2024-10-10 13:40:35 +0530

    rectify bsc addresses

--- config/protocols/bridges/bsc.ts
+++ config/protocols/bridges/bsc.ts
@@ -3,75 +3,98 @@ import { BRIDGES } from '../bridges'
 export const BSC_MAINNET = {
   bridges: {
     [BRIDGES.allbridge]: {
-      address: ['0x1234567890abcdef1234567890abcdef12345678'],
-      selectorInfo: [],
-    },
-    [BRIDGES.hop]: {
-      address: ['0xabcdefabcdefabcdefabcdefabcdefabcdefabcdef'],
+      address: ['0x3C4FA639c8D7E65c603145adaD8bD12F2358312f'],
       selectorInfo: [],
     },
     [BRIDGES.lifi]: {
-      address: ['0xabcdefabcdefabcdefabcdefabcdefabcdefabcdef'],
-      selectorInfo: [],
+      address: ['0x1231DEB6f5749EF6cE6943a275A1D3E7486F4EaE'],
+      selectorInfo: [
+        {
+          function: '',
+          functionSig: '',
+          offset: 0,
+        },
+      ],
     },
     [BRIDGES.socket]: {
-      address: ['0xabcdefabcdefabcdefabcdefabcdefabcdefabcdef'],
+      address: ['0x3a23F943181408EAC424116Af7b7790c94Cb97a5'],
       selectorInfo: [],
     },
     [BRIDGES.unizen]: {
-      address: ['0xabcdefabcdefabcdefabcdefabcdefabcdefabcdef'],
+      address: [
+        '0x880E0cE34F48c0cbC68BF3E745F17175BA8c650e',
+        '0xa9c430de6a91132330A09BE41f9f19bf45702f74',
+      ],
       selectorInfo: [],
     },
     [BRIDGES.xyFinance]: {
-      address: ['0xabcdefabcdefabcdefabcdefabcdefabcdefabcdef'],
+      address: [
+        '0xDF921bc47aa6eCdB278f8C259D6a7Fef5702f1A9',
+        '0x7D26F09d4e2d032Efa0729fC31a4c2Db8a2394b1',
+      ],
       selectorInfo: [],
     },
     [BRIDGES.deBridge]: {
-      address: ['0xabcdefabcdefabcdefabcdefabcdefabcdefabcdef'],
+      address: [
+        '0x663DC15D3C1aC63ff12E45Ab68FeA3F0a883C251',
+        '0x43dE2d77BF8027e25dBD179B491e8d64f38398aA',
+        '0xeF4fB24aD0916217251F553c0596F8Edc630EB66',
+      ],
       selectorInfo: [],
     },
     [BRIDGES.mayanFinance]: {
-      address: ['0xabcdefabcdefabcdefabcdefabcdefabcdefabcdef'],
+      address: [
+        '0xF3f04555f8FdA510bfC77820FD6eB8446f59E72d',
+        '0x0654874eb7F59C6f5b39931FC45dC45337c967c3',
+      ],
       selectorInfo: [],
     },
     [BRIDGES.omniBtc]: {
-      address: ['0xabcdefabcdefabcdefabcdefabcdefabcdefabcdef'],
+      address: ['0x2967E7Bb9DaA5711Ac332cAF874BD47ef99B3820'],
       selectorInfo: [],
     },
     [BRIDGES.rango]: {
-      address: ['0xabcdefabcdefabcdefabcdefabcdefabcdefabcdef'],
+      address: ['0x69460570c93f9DE5E2edbC3052bf10125f0Ca22d'],
       selectorInfo: [],
     },
     [BRIDGES.symbiosis]: {
-      address: ['0xabcdefabcdefabcdefabcdefabcdefabcdefabcdef'],
+      address: [
+        '0x10ED43C718714eb63d5aA57B78B54704E256024E',
+        '0x5c97D726bf5130AE15408cE32bc764e458320D2f',
+        '0x44487a445a7595446309464a82244b4bd4e325d5',
+        '0xb8f275fBf7A959F4BCE59999A2EF122A099e81A8',
+        '0x44b5d0F16Ad55c4e7113310614745e8771b963bB',
+      ],
       selectorInfo: [],
     },
     [BRIDGES.telosBridge]: {
-      address: ['0xabcdefabcdefabcdefabcdefabcdefabcdefabcdef'],
+      address: ['0x9c5ebCbE531aA81bD82013aBF97401f5C6111d76'],
       selectorInfo: [],
     },
     [BRIDGES.mesonFinance]: {
-      address: ['0xabcdefabcdefabcdefabcdefabcdefabcdefabcdef'],
+      address: ['0x25aB3Efd52e6470681CE037cD546Dc60726948D3'],
       selectorInfo: [],
     },
     [BRIDGES.okx]: {
-      address: ['0xabcdefabcdefabcdefabcdefabcdefabcdefabcdef'],
+      address: [
+        '0xFc99f58A8974A4bc36e60E2d490Bb8D72899ee9f',
+        '0x2c34A2Fb1d0b4f55de51E1d0bDEfaDDce6b7cDD6',
+      ],
       selectorInfo: [],
     },
     [BRIDGES.routerNitro]: {
-      address: ['0xabcdefabcdefabcdefabcdefabcdefabcdefabcdef'],
+      address: [
+        '0x2F301d3b045544A9D7Ec3FA090CD78986F11f2E7',
+        '0x260687eBC6C55DAdd578264260f9f6e968f7B2A5',
+      ],
       selectorInfo: [],
     },
     [BRIDGES.squid]: {
-      address: ['0xabcdefabcdefabcdefabcdefabcdefabcdefabcdef'],
-      selectorInfo: [],
-    },
-    [BRIDGES.router]: {
-      address: ['0xabcdefabcdefabcdefabcdefabcdefabcdefabcdef'],
+      address: ['0xce16F69375520ab01377ce7B88f5BA8C48F8D666'],
       selectorInfo: [],
     },
     [BRIDGES.stargate]: {
-      address: ['0xabcdefabcdefabcdefabcdefabcdefabcdefabcdef'],
+      address: ['0x138EB30f73BC423c6455C53df6D89CB01d9eBc63'],
       selectorInfo: [],
     },
     [BRIDGES.synapse]: {

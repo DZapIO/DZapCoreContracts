63684b63d41e5e9dc2db014fc002321fb48ec00b
dzaptech <dezaptech@gmail.com>
2024-10-10 14:45:47 +0530

    whitelist synapse on linea

--- config/protocols/bridges/linea.ts
+++ config/protocols/bridges/linea.ts
@@ -25,19 +25,19 @@ export const LINEA_MAINNET = {
       ],
       selectorInfo: [],
     },
-    [BRIDGES.routerNitro]: {
-      address: ['0x6D6050Ca1dd8e4aAb9164B663d805104a3ECFC34'],
-      selectorInfo: [],
-    },
-    [BRIDGES.xyFinance]: {
-      address: ['0x73Ce60416035B8D7019f6399778c14ccf5C9c7A1'],
-      selectorInfo: [],
-    },
     [BRIDGES.okx]: {
       address: [
         '0xf956D9FA19656D8e5219fd6fa8bA6cb198094138',
         '0x57df6092665eb6058DE53939612413ff4B09114E',
       ],
+      selectorInfo: [],
+    },
+    [BRIDGES.rango]: {
+      address: ['0x69460570c93f9DE5E2edbC3052bf10125f0Ca22d'],
+      selectorInfo: [],
+    },
+    [BRIDGES.routerNitro]: {
+      address: ['0x6D6050Ca1dd8e4aAb9164B663d805104a3ECFC34'],
       selectorInfo: [],
     },
     [BRIDGES.socket]: {
@@ -46,6 +46,10 @@ export const LINEA_MAINNET = {
     },
     [BRIDGES.squid]: {
       address: ['0xce16F69375520ab01377ce7B88f5BA8C48F8D666'],
+      selectorInfo: [],
+    },
+    [BRIDGES.stargate]: {
+      address: ['0x81F6138153d473E8c5EcebD3DC8Cd4903506B075'],
       selectorInfo: [],
     },
     [BRIDGES.symbiosis]: {
@@ -57,12 +61,16 @@ export const LINEA_MAINNET = {
       ],
       selectorInfo: [],
     },
-    [BRIDGES.stargate]: {
-      address: ['0x81F6138153d473E8c5EcebD3DC8Cd4903506B075'],
+    [BRIDGES.synapse]: {
+      address: [
+        '0x7E7A0e201FD38d3ADAA9523Da6C109a07118C96a',
+        '0xd5a597d6e7ddf373a92C8f477DAAA673b0902F48',
+        '0x00cD000000003f7F682BE4813200893d4e690000',
+      ],
       selectorInfo: [],
     },
-    [BRIDGES.rango]: {
-      address: ['0x69460570c93f9DE5E2edbC3052bf10125f0Ca22d'],
+    [BRIDGES.xyFinance]: {
+      address: ['0x73Ce60416035B8D7019f6399778c14ccf5C9c7A1'],
       selectorInfo: [],
     },
   },

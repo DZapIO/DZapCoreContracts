f86dd624993b827d64df8652fb4e7749e038813d
dante055 <67709458+dante055@users.noreply.github.com>
2025-03-06 19:12:14 +0530

    added zeroX to polygon

--- config/protocols/dexes/polygon.ts
+++ config/protocols/dexes/polygon.ts
@@ -19,7 +19,8 @@ export const POLYGON_DEXES = {
     [DEXES.zeroX]: [
       '0xdef1c0ded9bec7f1a1670819833240f027b25eff',
       '0xfF4b330c5BC3811b66d8864CF8078D8F2db20Dd6',
-      '0x7f20a7A526D1BAB092e3Be0733D96287E93cEf59',
+      '0x7f20a7A526D1BAB092e3Be0733D96287E93cEf59', // Polygon settler
+      '0x0000000000001ff3684f28c67538d4d072c22734', // Allowance Holder
     ],
     [DEXES.bebop]: [
       '0xBeB09000fa59627dc02Bb55448AC1893EAa501A5',

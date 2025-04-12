8aac50fc86f7e9a4c7702cb289cec96f890fcb48
dante055 <67709458+dante055@users.noreply.github.com>
2025-04-12 12:29:56 +0530

    added zkfair integrator

--- config/integrators.ts
+++ config/integrators.ts
@@ -1224,4 +1224,25 @@ export const INTEGRATOR_CONFIG = {
       ],
     },
   },
+  [CHAIN_IDS.ZKFAIR]: {
+    [INTEGRATORS.DZAP]: {
+      address: '0x45dd5ec28dbf5296dffae428d7f484ab02ee7bad',
+      fee: [
+        {
+          type: 1,
+          tokenFee: 0,
+          fixedNativeFeeAmount: 0,
+          dzapTokenShare: 0,
+          dzapFixedNativeShare: 0,
+        },
+        {
+          type: 0,
+          tokenFee: 0,
+          fixedNativeFeeAmount: 0.35,
+          dzapTokenShare: 0,
+          dzapFixedNativeShare: 0,
+        },
+      ],
+    },
+  },
 }

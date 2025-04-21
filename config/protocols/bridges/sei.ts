f16d08b8b6c1b0acab8139fdf2aed7f3ae734769
dzaptech <dezaptech@gmail.com>
2025-04-21 11:41:18 +0530

    WL bridges and dexes on sei & merlin

--- config/protocols/bridges/sei.ts
+++ config/protocols/bridges/sei.ts
@@ -5,6 +5,9 @@ export const SEI_BRIDGES = {
     [BRIDGES.lifi]: {
       address: ['0x1231DEB6f5749EF6cE6943a275A1D3E7486F4EaE'],
     },
+    [BRIDGES.stargate]: {
+      address: ['0x45d417612e177672958dC0537C45a8f8d754Ac2E'],
+    },
     [BRIDGES.symbiosis]: {
       address: [
         '0xcE8f24A58D85eD5c5A6824f7be1F8d4711A0eb4C',
--- config/protocols/dexes/merlin.ts
+++ config/protocols/dexes/merlin.ts
@@ -6,5 +6,6 @@ export const MERLIN_DEXES = {
       '0x127a986cE31AA2ea8E1a6a0F0D5b7E5dbaD7b0bE',
       '0x8b773D83bc66Be128c60e07E17C8901f7a64F000',
     ],
+    [DEXES.openOcean]: ['0x6352a56caadc4f1e25cd6c75970fa768a3304e64'],
   },
 }

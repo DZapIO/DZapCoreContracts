39eb3c44c07c5b171c81d3b43f5e0655b6838873
dante055 <sonofsparda055@gmail.com>
2025-07-09 14:51:32 +0530

    added bridges to arbitrumSepolia

---
+++ config/protocols/bridges/arbitrumSepolia.ts
@@ -0,0 +1,6 @@
+import { BRIDGES } from '../bridgeNames'
+
+export const ARBITRUM_SEPOLIA_BRIDGES = {
+  [BRIDGES.across]: ['0x7E63A5f1a8F0B4d0934B2f2327DAED3F6bb2ee75'],
+  [BRIDGES.cctp]: ['0x9f3B8679c73C2Fef8b59B4f3444d4e156fb70AA5'],
+}
